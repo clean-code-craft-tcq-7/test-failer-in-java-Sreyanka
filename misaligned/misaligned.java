717b613583190686b48a52fa8d853077b49dcfdf
Sreyanka <35264560+Sreyanka@users.noreply.github.com>
2022-11-27 17:37:31 +0530

    add test case

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -5,14 +5,23 @@ public class Misaligned {
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+                System.out.println(printAlignment(i,j,minorColors[i],majorColors[i]));
             }
         }
         return i * j;
     }
+    
+    public static string printAlignment(int i,int j,String minorColor,String majorColor){
+        String number = Integer.toString(i * 5 + j);
+    	String result = number.concat(" | " + majorColor + " | " + minorColor);
+    	return result;
+    }
+    
     public static void main(String[] args) { 
         int result = printColorMap();
         assert(result == 25);
+        String result = printAlignment(2, 5, "White", "Blue");
+		assert result.equals("15 | White | Blue");
         System.out.println("All is well (maybe!)");
     }
 }
