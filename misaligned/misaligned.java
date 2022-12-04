d303a05cd5e34a61c99c809017b7f6269273903c
Sreyanka <35264560+Sreyanka@users.noreply.github.com>
2022-12-04 17:49:28 +0530

    single digit test cases

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,27 +1,39 @@
 public class Misaligned {
-    static int printColorMap() {
+   static int printColorMap() {
         String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
         String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
-                System.out.println(printAlignment(i,j,minorColors[i],majorColors[i]));
+            	if (i * 5 + j < 10) {
+            		System.out.println(singlePrintAlignment(i,j, majorColors[i], minorColors[j]));
+            	} else {
+            		System.out.println(doublePrintAlignment(i,j, majorColors[i], minorColors[j]));
+            	}
             }
         }
         return i * j;
     }
     
-    public static String printAlignment(int i,int j,String minorColor,String majorColor){
+    public static String doublePrintAlignment(int i,int j,String minorColor,String majorColor){
         String number = Integer.toString(i * 5 + j);
     	String result = number.concat(" | " + majorColor + " | " + minorColor);
     	return result;
     }
+	
+ public static String singlePrintAlignment(int i,int j,String minorColor,String majorColor){
+        String number = Integer.toString(i * 5 + j);
+    	String result = number.concat("  | " + majorColor + " | " + minorColor);
+    	return result;
+    }		
     
     public static void main(String[] args) { 
         int result = printColorMap();
         assert(result == 25);
-        String resultAligned = printAlignment(2, 5, "White", "Blue");
-	assert(resultAligned.equals("15 | White | Blue"));
+        String resultAlignedDouble = doublePrintAlignment(2, 5, "White", "Blue");
+	assert(resultAlignedDouble.equals("15 | White | Blue"));
+	String resultAlignedSingle = singlePrintAlignment(2, 3, "White", "Blue");
+	assert(resultAlignedSingle.equals("9  | White | Blue"));
         System.out.println("All is well (maybe!)");
     }
 }
