bc4179f0c93dcd3f3f03cc9ab9e83edc6ff306dc
Sreyanka <35264560+Sreyanka@users.noreply.github.com>
2022-12-04 17:13:33 +0530

    refactor

--- tshirts/tshirtsTest.java
+++ tshirts/tshirtsTest.java
@@ -6,21 +6,21 @@ class tshirtsTest {
 	void smallSize() {
 		for (int i = 0; i < 10; i++) {
 			int small = (int) (Math.random() * 37);
-			assert(Tshirts.size(small) == "S");
+			assert(tshirts.size(small) == "S");
 		}
-		assert(Tshirts.size(1) == "S");
-		assert(Tshirts.size(15) == "S");
-		assert(Tshirts.size(37) == "S");
+		assert(tshirts.size(1) == "S");
+		assert(tshirts.size(15) == "S");
+		assert(tshirts.size(37) == "S");
 	}
 	
 	@Test
 	void mediumSize() {
-		assert(Tshirts.size(40) == "M");
-        assert(Tshirts.size(41) == "M");
-        assert(Tshirts.size(38) == "M");
+		assert(tshirts.size(40) == "M");
+        assert(tshirts.size(41) == "M");
+        assert(tshirts.size(38) == "M");
 		for (int i = 0; i < 10; i++) {
 			int medium = (int) (Math.random() * (41-38) + 38);
-			assert(Tshirts.size(medium) == "M");
+			assert(tshirts.size(medium) == "M");
 		}
 	}
 	
@@ -28,11 +28,11 @@ class tshirtsTest {
 	void largeSize() {
 		for (int i = 0; i < 10; i++) {
 			int large = (int) (Math.random() * (100-42) + 42);
-			assert(Tshirts.size(large) == "L");
+			assert(tshirts.size(large) == "L");
 		}
-		    assert(Tshirts.size(42) == "L");
-        assert(Tshirts.size(60) == "L");
-        assert(Tshirts.size(100) == "L");
+		    assert(tshirts.size(42) == "L");
+        assert(tshirts.size(60) == "L");
+        assert(tshirts.size(100) == "L");
 	}
 
 }
